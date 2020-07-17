5a9f7932a5b1f09ec583e5855b49b9d13dd63174
sai <saiprakash15may@gmail.com>
2020-07-17 21:38:41 +0530

    123

--- tute1.py
+++ tute1.py
@@ -1,14 +1,24 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+import json
 from  datetime import datetime
 
+
+with open('config.json','r') as c:
+    params = json.load(c)["params"]
+
+
+local_server = True
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/puppysite'
+if(local_server):
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['local_uri']
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['prod_uri']
 db = SQLAlchemy(app)
 
 
 class Contacts(db.Model):
-    sno=db.Column(db.Integer,primary_key=True)
+    sno  =  db.Column(db.Integer,primary_key=True)
     name = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(20), nullable=False)
     phone_num = db.Column(db.String(12), nullable=False)
@@ -18,15 +28,15 @@ class Contacts(db.Model):
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template('index.html',params=params)
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', params=params)
 
 @app.route("/post")
 def SamplePost():
-    return render_template('post.html')
+    return render_template('post.html', params=params)
 
 @app.route("/contact", methods = ['GET','POST'])
 def contact():
@@ -36,10 +46,10 @@ def contact():
         phone = request.form.get('phone')
         message = request.form.get('message')
 
-        entry = Contacts(name=name,email=email, phone_num=phone,mes=message,date=datetime.now())
+        entry = Contacts(name=name, email=email, phone_num=phone, mes=message, date=datetime.now())
         db.session.add(entry)
         db.session.commit()
-    return render_template('contact.html')
+    return render_template('contact.html', params=params)
 
 app.run(debug=True)
 
